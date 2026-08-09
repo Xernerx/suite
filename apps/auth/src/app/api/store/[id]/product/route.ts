59018f33d4af6146c6e256b982d8eb1ee44879c7
Dummi <dummi@xernerx.com>
2026-08-09 15:22:06 +0200

    chore: update actions

--- apps/auth/src/app/api/store/[id]/product/route.ts
+++ apps/auth/src/app/api/store/[id]/product/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+const stripe = new Stripe((process.env.STRIPE_SECRET_KEY as string) || 'sk_test_placeholder', {
 	apiVersion: '2026-07-29.dahlia',
 });
 
